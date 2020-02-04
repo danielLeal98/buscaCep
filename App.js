a663668136bf768f5b314051294cae77737663d8
Daniel Matheus <37132172+danielLeal98@users.noreply.github.com>
2020-02-04 17:47:41 -0200

    BuscaCep
    Aplicação que realiza busca de endereço por CEP consumindo a API do ViaCep.

---
+++ App.js
@@ -0,0 +1,158 @@
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  ImageBackground,
+  Button,
+  Alert,
+  Image,
+  TouchableOpacity,
+  TextInput
+} from 'react-native';
+
+import ViaCep from 'react-via-cep';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+export default class App extends Component {
+
+  constructor(props){
+        super(props);
+        this.state = {text: '', cep: '', localidade: '', uf: '', logradouro: '', bairro: ''};
+        this.filterText = this.filterText.bind(this);
+        this.capturaCep = this.capturaCep.bind(this);
+        this.limparDados = this.limparDados.bind(this);        
+    }
+
+    filterText(text) {
+      this.setState({
+          text: text.replace(/([^\d\s/-])/g, '')
+      });
+    }
+
+  capturaCep(){           
+    fetch(`https://viacep.com.br/ws/${this.state.text}/json/`)    
+          .then((r)=>r.json())
+          .then((json)=>{
+            let s = this.state;
+            s.localidade = json.localidade;
+            s.cep = json.cep;
+            s.uf = json.uf;
+            s.logradouro = json.logradouro;
+            s.bairro = json.bairro;
+            this.setState(s);            
+          });
+  }
+
+  limparDados(){
+    let s = this.state;
+    s.text = '';
+    s.cep = '';
+    s.localidade = '';
+    s.uf = '';
+    s.logradouro = '';
+    s.bairro = ''
+    this.setState(s);
+  }
+
+  render(){
+    return (                        
+      <View style={styles.body}>
+        <ImageBackground source={require('../buscaCep/imagens/background3.png')} style={styles.bgImage}>
+        <View style={styles.sectionContainer}>
+          <Text style={styles.sectionTitle}>Busca Cep</Text>              
+        </View>      
+        <View style={styles.sectionContainer}>
+          <TextInput style={styles.textInputStyle}  onChangeText={this.filterText} value={this.state.text} placeholder="Insira o CEP desejado" placeholderTextColor="#121111"/>  
+        </View>  
+        <View style={styles.sectionContainer}>  
+        <Button title="Buscar" style={styles.button} accessibilityLabel="Buscar" onPress={this.capturaCep} color="#5cb85c"/>              
+        </View> 
+        <View style={styles.infoEndereco}>
+          <Text style={styles.titulo1}>{this.state.localidade}</Text>
+          <Text style={styles.titulo2}> {this.state.logradouro} - {this.state.bairro} - {this.state.uf}</Text> 
+          <Text style={styles.titulo3}> </Text>          
+        </View>        
+        <View style={styles.viewBotao}>
+          <Button title="Limpar" onPress={this.limparDados} color="#ff3232"/>      
+          <Button title="Salvar" onPress={() => Alert.alert('Salvar Consulta')}   color="#5cb85c"/>                      
+          <TouchableOpacity onPress={() => Alert.alert('Chama Google Maps')}>
+            <Image source={require('../buscaCep/imagens/google-maps.png')} style={styles.imagemMaps}/>
+          </TouchableOpacity>
+        </View> 
+        </ImageBackground>     
+      </View>          
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: 'transparent',
+    flex:1
+  },
+  bgImage:{
+    flex: 1,
+    width: null
+
+  },
+  sectionContainer: {
+    marginTop: 20,
+    paddingHorizontal: 24,
+    justifyContent: 'center'
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#121111',
+    textAlign: 'center'
+  },
+  textInputStyle: {  
+    borderColor: '#121111',        
+    height: 40,  
+    margin: 20,  
+    padding: 10, 
+    alignContent:'center',
+    borderWidth : 1,
+    fontSize:17,
+    textAlign: 'center'
+  },
+  infoEndereco: {            
+    alignItems:'center',
+    marginTop: 40
+  },
+  viewBotao: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginLeft:10,
+    position: 'absolute',
+    left: 40, 
+    right: 40, 
+    bottom: 40,
+    marginRight: 10
+  },
+  imagemMaps:{
+    width: 40,
+    height:40
+  },
+  titulo1:{
+    fontWeight:'bold',
+    fontSize:25
+  },
+  titulo2:{    
+    fontSize:20
+  },
+  titulo3:{
+    fontSize:20
+  }
+});
